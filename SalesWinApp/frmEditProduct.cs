8d5dd318bcf50e161b7fa737a33b2fe39ca7716d
PhongNT <nguyenthaiphong258@gmail.com>
2022-07-02 22:57:40 +0700

    Add New, Update, Delete And View Order

--- SalesWinApp/frmEditProduct.cs
+++ SalesWinApp/frmEditProduct.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessObject;
+using DataAccess;
+using DataAccess.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,15 +19,108 @@ namespace SalesWinApp
         {
             InitializeComponent();
         }
+        ProductRepository productRepository = new ProductRepository();
+        public bool InsertOrUpdate { get; set; }
+
+        public Product productInfo { get; set; }
 
         private void btn_Save_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                bool validation = true;
+                if (txt_CategoryID.Text.Trim().Length == 0)
+                {
+                    validation = false;
+                    errorProviderCategoryID.SetError(txt_CategoryID, "Please input the category id!");
+                }
+                else
+                {
+                    errorProviderCategoryID.SetError(txt_CategoryID, "");
+                }
+                if (txt_ProductName.Text.Trim().Length == 0)
+                {
+                    validation = false;
+                    errorProviderProductName.SetError(txt_ProductName, "Please input the product name!");
+                }
+                else
+                {
+                    errorProviderProductName.SetError(txt_ProductName, "");
+                }
+                if (txt_Weight.Text.Trim().Length == 0)
+                {
+                    validation = false;
+                    errorProviderWeight.SetError(txt_Weight, "Please input the weight!");
+                }
+                else
+                {
+                    errorProviderWeight.SetError(txt_Weight, "");
+                }
+                if (txt_UnitPrice.Text.Trim().Length == 0)
+                {
+                    validation = false;
+                    errorProviderUnitPrice.SetError(txt_UnitPrice, "Please input the unit price!");
+                }
+                else
+                {
+                    errorProviderUnitPrice.SetError(txt_UnitPrice, "");
+                }
+                if (txt_UnitsInStock.Text.Trim().Length == 0)
+                {
+                    validation = false;
+                    errorProviderUnitsInStock.SetError(txt_UnitsInStock, "Please input the unit in stock!");
+                }
+                else
+                {
+                    errorProviderUnitsInStock.SetError(txt_UnitsInStock, "");
+                }
+                if (validation)
+                {
+                    var product = new Product
+                    {
+                        CategoryId = int.Parse(txt_CategoryID.Text),
+                        ProductName = txt_ProductName.Text,
+                        Weight = txt_Weight.Text,
+                        UnitPrice = decimal.Parse(txt_UnitPrice.Text),
+                        UnitInStock = int.Parse(txt_UnitsInStock.Text),
+                    };
+                    if (InsertOrUpdate == false)
+                    {
+                        productRepository.AddNewProduct(product);
+                    }
+                    else
+                    {
+                        product.ProductId = int.Parse(txt_ProductID.Text);
+                        productRepository.UpdateProduct(product);
+                    }
+                    this.DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    this.DialogResult = DialogResult.None;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, InsertOrUpdate == false ? "Add new product" : "Update product");
+            }
         }
 
-        private void btn_Cancel_Click(object sender, EventArgs e)
-        {
+        private void btn_Cancel_Click(object sender, EventArgs e) => Close();
 
+        private void frmEditProduct_Load(object sender, EventArgs e)
+        {
+            var id = productRepository.GetProducts().Max(c => c.ProductId) + 1;
+            txt_ProductID.Text = id.ToString();
+            if (InsertOrUpdate == true)
+            {
+                txt_ProductID.Text = productInfo.ProductId.ToString();
+                txt_CategoryID.Text = productInfo.CategoryId.ToString();
+                txt_ProductName.Text = productInfo.ProductName;
+                txt_Weight.Text = productInfo.Weight;
+                txt_UnitPrice.Text = productInfo.UnitPrice.ToString();
+                txt_UnitsInStock.Text = productInfo.UnitInStock.ToString();
+            }
         }
     }
 }
--- SalesWinApp/frmProductManagement.cs
+++ SalesWinApp/frmProductManagement.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessObject;
+using DataAccess;
+using DataAccess.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,11 +20,100 @@ namespace SalesWinApp
             InitializeComponent();
         }
 
-        private void btnClose_Click(object sender, EventArgs e)
-        {
+        ProductRepository productRepository = new ProductRepository();
 
+        private void LoadProducts()
+        {
+            var products = ProductDAO.Instance.GetProducts();
+
+            txtProductID.DataBindings.Clear();
+            txtCategoryID.DataBindings.Clear();
+            txtProductName.DataBindings.Clear();
+            txtWeight.DataBindings.Clear();
+            txtUnitPrice.DataBindings.Clear();
+            txtUnitsInStock.DataBindings.Clear();
+
+            txtProductID.DataBindings.Add("Text",  products, "ProductID");
+            txtCategoryID.DataBindings.Add("Text", products, "CategoryID");
+            txtProductName.DataBindings.Add("Text", products, "ProductName");
+            txtWeight.DataBindings.Add("Text", products, "Weight");
+            txtUnitPrice.DataBindings.Add("Text", products, "UnitPrice");
+            txtUnitsInStock.DataBindings.Add("Text", products, "UnitInStock");
+
+            dgv_ProductList.DataSource = products;
+
+            if(products.Count > 0)
+            {
+                btn_DeleteProduct.Enabled = true;
+            }
+            else
+            {
+                btn_DeleteProduct.Enabled = false;
+            }
         }
 
-       
+        private void btnClose_Click(object sender, EventArgs e) => Close();
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            frmEditProduct frmEditProduct = new frmEditProduct
+            {
+                Text = "Add new product",
+                InsertOrUpdate = false
+            };
+            if (frmEditProduct.ShowDialog() == DialogResult.OK)
+            {
+                LoadProducts();
+            }
+        }
+
+        private void frmProductManagement_Load(object sender, EventArgs e) => LoadProducts();
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                productRepository.DeleteProduct(int.Parse(txtProductID.Text));
+                LoadProducts();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Delete Product");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            frmEditProduct frmEditProduct = new frmEditProduct
+            {
+                Text = "Update product",
+                InsertOrUpdate = true,
+                productInfo = GetProductObject()
+            };
+            if (frmEditProduct.ShowDialog() == DialogResult.OK)
+            {
+                LoadProducts();
+            }
+        }//end btnUpdate_Click
+
+        private Product GetProductObject()
+        {
+            Product product = null;
+            try
+            {
+                product = new Product
+                {
+                    ProductId = int.Parse(txtProductID.Text),
+                    CategoryId = int.Parse(txtCategoryID.Text),
+                    ProductName = txtProductName.Text,
+                    Weight = txtWeight.Text,
+                    UnitPrice = decimal.Parse(txtUnitPrice.Text),
+                    UnitInStock = int.Parse(txtUnitsInStock.Text)
+                };
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Get Product");
+            }
+            return product;
+        }
     }
 }
